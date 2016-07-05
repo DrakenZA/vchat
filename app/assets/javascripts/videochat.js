51e7dd98b047448fc84e6e45aa6690248b5bceac
David Weiner <davidweiner@Davids-MacBook-Air.local>
2016-07-05 17:15:12 +0200

    making peer.js SSL

--- app/assets/javascripts/videochat.js
+++ app/assets/javascripts/videochat.js
@@ -11,7 +11,7 @@ $(document).on('ready page:load', function(){
 },function(){ });
 
 
-var peer = new Peer({key: '9ejxboga34p74x6r'});
+var peer = new Peer({key: '9ejxboga34p74x6r'},{secure:true});
 
 
 peer.on('open', function(id) {
